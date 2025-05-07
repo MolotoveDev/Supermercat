ceed675e42cfd80fc78e7ff5a7975df2ed6b7329
MolotoveDev <alex.carrasco005@gmail.com>
2025-05-07 13:23:23 +0200

    Created class + inited some attributes
    Needed other classes in order to make the supermarket class work properly

---
+++ Supermarket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermercat
+{
+    public class Supermarket
+    {
+        #region Attributes
+        
+        string name;
+        string address;
+        public static int MAXLINES = 5;
+        int activeLines;
+        //TODO: Add checkout class.
+
+        #endregion
+    }
+}
