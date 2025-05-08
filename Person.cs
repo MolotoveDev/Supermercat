dd9d8a7e3aefbfbc244b7dee20107f9c034ddbc7
Houssamess05 <houssam.essarhiar@gmail.com>
2025-05-08 12:20:44 +0200

    Finished person class
    Implemented all the methods, propertys, attributes and everything required on the documentation.

--- Person.cs
+++ Person.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Reflection.Metadata;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+using static Supermercat.Person;
 
 namespace Supermercat
 {
@@ -24,7 +30,7 @@ namespace Supermercat
         {
             get { return _fullNanme; }
         }
-        public abstract decimal GetRating { get; }
+        public abstract double GetRating { get; }
         /// <summary>
         /// First constructor.
         /// </summary>
@@ -77,5 +83,88 @@ namespace Supermercat
             return this.GetRating.CompareTo(other.GetRating);
         }
 
+        public class Customer : Person
+        {
+            private int? _fidelity_card;
+
+            /// <summary>
+            /// Creates a new customer initializing the base class attributes + fidelity card attribute.
+            /// </summary>
+            /// <param name="id">Id from the new costumer</param>
+            /// <param name="fullName">fullName from the new costumer</param>
+            /// <param name="fidelityCard">fidelity card from the new costumer</param>
+            public Customer(string id, string fullName, int? fidelityCard) : base(id, fullName)
+            {
+                _fidelity_card = fidelityCard;
+                _id = id;
+                _fullNanme= fullName;
+            }
+            /// <summary>
+            /// The property calculates the rating of a customer according to the gross amount 
+            /// of his/her purchases.The rating is the 2% of the customer’s purchases
+            /// </summary>
+            public override double GetRating => (2 / _totalInvoiced) * 100;
+
+
+            /// <summary>
+            /// adds the points parameter to the current points of the customer 
+            /// except if the customer hasn't a fidelity card.In this case points are lost (no added to anything)
+            /// </summary>
+            /// <param name="pointsToAdd">points to add</param>
+            public override void AddPoints(int pointsToAdd)
+            {
+                if(_id != "CASH")
+                {
+                    _points += pointsToAdd;
+                }
+            }
+            public override string ToString()
+            {
+                return $"DNI/NIE-> {_id} NOM-> {_fullNanme} RATING-> {GetRating} vendes-> {_totalInvoiced}€ PUNTS->{_points} DISPONIBLE-> {base.ToString()}";
+            }
+        }
+        public class Cashier : Person
+        {
+           
+            private DateTime _joiningDate;
+            /// <summary>
+            /// the complete years of service in the supermarket the cashier was hired.
+            /// </summary>
+            public int YearOfService
+            {
+                get
+                {
+                    return (DateTime.Today - _joiningDate).Days / 365;
+                }
+            }
+            /// <summary>
+            /// Creates a new cashier initializing the base class attributes + joining date attribute.
+            /// </summary>
+            /// <param name="id"></param>
+            /// <param name="fullName"></param>
+            /// <param name="joining_Date"></param>
+            public Cashier(string id, string fullName,DateTime joining_Date) : base(id, fullName)
+            {
+                _joiningDate = joining_Date;
+                _id = id;
+                _fullNanme = fullName;
+            }
+            /// <summary>
+            /// 
+            /// </summary>
+            public override double GetRating => (10 / _joiningDate.Day) * 100;
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="pointsToAdd"></param>
+            public override void AddPoints(int pointsToAdd)
+            {
+               _points += (YearOfService + 1) * pointsToAdd;
+            }
+            public override string ToString()
+            {
+                return $"DNI/NIE-> {_id} NOM-> {_fullNanme} RATING-> {GetRating} ANTIGUITAT-> {YearOfService} VENDES-> {_totalInvoiced}€ PUNTS->{_points} DISPONIBLE-> {base.ToString()}";
+            }
+        }
     }
 }
